7c31a4194a7dec30999f464f6aef70613e6d8555
Soraku7 <2673140287@qq.com>
2023-07-04 14:42:38 +0800

    115

--- leetcode/editor/cn/[115]不同的子序列.cpp
+++ leetcode/editor/cn/[115]不同的子序列.cpp
@@ -44,14 +44,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
+    
     int numDistinct(string s, string t) {
-        for(int i = 1 ; i <= s.size() ; i ++){
-            for(int j = 1 ; j <= t.size() ; j ++){
-                if(s[i - 1] == s[j - 1]){
-                
-                }
+        int n = s.size(), m = t.size();
+        s = " " + s;
+        t = " " + t;
+        vector <vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i < n; i++) dp[i][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (s[i] == t[j]) dp[i][j] = (0LL + dp[i][j] + dp[i - 1][j - 1]) % INT_MAX;
             }
         }
+        return dp[n][m];
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
